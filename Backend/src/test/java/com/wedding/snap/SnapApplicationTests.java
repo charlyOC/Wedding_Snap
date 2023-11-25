7d3994acad64e73f8daae118584f82afe663c36e
charly <charly.dewalles@gmail.com>
2023-11-25 20:41:07 +0100

    add back

---
+++ Backend/src/test/java/com/wedding/snap/SnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedding.snap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
